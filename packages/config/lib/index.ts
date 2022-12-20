268d1f177fc0b4ce89db86818240bb7bd7eeb8da
Pavel Volkov <volkov030@gmail.com>
2022-12-20 11:58:22 +0300

    Add export of request-update-provider

--- packages/config/lib/index.ts
+++ packages/config/lib/index.ts
@@ -1,6 +1,7 @@
 export * from './module';
 export { Config, InjectConfig } from './decorators';
 export { getConfigToken } from './nestjs-getters';
+export { RequestUpdateProvider } from './request-update-provider';
 
 export { IPlugin, UseAsyncPlugin, IAsyncPlugin } from './plugin';
 export * from './decorator-makers';
